5415f9e047cdc8c80c92480e8b86a268bf465ece
Ruchit Modi <modi.ruchit6@gmail.com>
2020-10-16 12:21:52 -0500

    Updates

---
+++ precache-manifest.be50983853461480ccb149f2d8a322d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52038734b7ad336b74ddad42dbfb5f39",
+    "url": "/magic-numbers/index.html"
+  },
+  {
+    "revision": "3ab4b9c1c894d5c0ac37",
+    "url": "/magic-numbers/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd8ad2340ae91bb0a25b",
+    "url": "/magic-numbers/static/js/2.c0036ed3.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/magic-numbers/static/js/2.c0036ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab4b9c1c894d5c0ac37",
+    "url": "/magic-numbers/static/js/main.8983453c.chunk.js"
+  },
+  {
+    "revision": "cbb598d538e8ec8ebbf4",
+    "url": "/magic-numbers/static/js/runtime-main.66890e4f.js"
+  }
+]);
